878ed016f3f48a0551871c6e3c81a2b0bb541cb6
Yasmin <143837036+Sange06@users.noreply.github.com>
2025-04-15 10:52:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,13 +14,6 @@ aumentaFonteBotao.addEventListener(‘click’, function(){
 tamanhoAtualFonte += 0.1;
 document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 }
-
-})
-<body>
-...
-<script src=”script.js”></script>
-
-</body>
 document.addEventListener(‘DOMContentLoaded’, function(){
 const aumentaFonteBotao = document.getElementById(‘aumentar-fonte’);
 const diminuirFonteBotao = document.getElementById(diminuir-fonte’);
