7e0028384505161932f3e52cea938b05aeaccc0a
Yasmin <143837036+Sange06@users.noreply.github.com>
2025-04-15 19:05:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-botaoDeAcessibilidade.addEventListener(‘click’, function (){
-botaoDeAcessibilidade.classList.toggle(‘rotacao-botao’);
-opcoesDeAcessibilidade.classList.toggle(‘apresenta-lista’);
-const botaoSelecionado = botaoDeAcessibilidade.getAttribute
-(‘aria-expanded’) === ‘true’;
-botaoDeAcessibilidade.setAttribute(‘aria-expanded’,
-!botaoSelecionado);
-})
-document.addEventListener(‘DOMContentLoaded’, function(){
-const aumentaFonteBotao = document. getElementById(‘aumentar-fonte’);
-et tamanhoAtualFonte = 1;
-
-aumentaFonteBotao.addEventListener(‘click’, function(){
-tamanhoAtualFonte += 0.1;
-document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-}
 document.addEventListener(‘DOMContentLoaded’, function(){
 const aumentaFonteBotao = document.getElementById(‘aumentar-fonte’);
 const diminuirFonteBotao = document.getElementById(diminuir-fonte’);
